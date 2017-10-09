10f6d4ae224b339fcd47aea4b6815c2899fb0b74
delacroix <unmask429@gmail.com>
2017-10-09 22:10:05 +0800

    class Dog

---
+++ class/dog.py
@@ -0,0 +1,16 @@
+class Dog():
+    """an easy trial to sim a dog"""
+    def __init__(self, name, age):
+        """init attribute name and age"""
+        self.name = name
+        self.age = age
+
+
+    def sit(self):
+        """sim a dog to sit down"""
+        print(self.name.title() + " is now sitting.")
+
+
+    def roll_over(self):
+        """sim a dog to roll over"""
+        print(self.name.title() + " rolled over!")
