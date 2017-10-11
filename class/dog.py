505e500ef7749ce6f48de89e8885a4285d63adce
delacroix <unmask429@gmail.com>
2017-10-11 20:23:54 +0800

    class Dog

--- class/dog.py
+++ class/dog.py
@@ -12,3 +12,14 @@ class Dog():
     def roll_over(self):
         """sim a dog to roll over"""
         print(self.name.title() + " rolled over!")
+
+my_dog = Dog('willie', 6)
+your_dog = Dog('lucy', 3)
+
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is " + str(my_dog.age) + " years old.")
+my_dog.sit()
+
+print("\nYour dog's name is " + your_dog.name.title() + ".")
+print("Your dog is " + str(your_dog.age) + " years old.")
+your_dog.roll_over()
