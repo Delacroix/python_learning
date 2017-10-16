83ba9092f573da84bc3483dda95fc75c2db9c50f
delacroix <unmask429@gmail.com>
2017-10-16 22:30:47 +0800

    car_class_1016

--- class/car2.py
+++ class/car2.py
@@ -12,6 +12,7 @@ class Car():
     def read_odometer(self):
         print("This car has " + str(self.odometer_reading) + " miles on it.")
 
-my_new_car = Car('audi', 'a4', '2016')
+my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
+my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
--- class/car3.py
+++ class/car3.py
@@ -5,13 +5,18 @@ class Car():
         self.year = year
         self.odometer_reading = 0
 
-    def get_descriptive_name(self):
+    def get_desciptive_name(self):
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
 
     def read_odometer(self):
         print("This car has " + str(self.odometer_reading) + " miles on it.")
 
+    def update_odometer(self, mileage):
+        self.odometer_reading = mileage
+
 my_new_car = Car('audi', 'a4', '2016')
-print(my_new_car.get_descriptive_name())
+print(my_new_car.get_desciptive_name())
+
+my_new_car.update_odometer(23)
 my_new_car.read_odometer()
