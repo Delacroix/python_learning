6dd7c0a58d3c55054bb4084f6746f08f0174fc0d
delacroix <unmask429@gmail.com>
2017-10-19 22:22:58 +0800

    electric_car_3

--- class/electric_car_3.py
+++ class/electric_car_3.py
@@ -23,6 +23,16 @@ class Battery():
     def describe_battery(self):
         print("This car has a " + str(self.battery_size) + "-kwh battery.")
 
+    def get_range(self):
+        if self.battery_size == 70:
+            total_range = 240
+        elif self.battery_size == 85:
+            total_range = 270
+
+        message = "This car can go approximately " + str(total_range)
+        message += " miles on a full charge."
+        print(message)
+
 
 class ElectricCar(Car):
     def __init__(self, make, model, year):
@@ -34,3 +44,4 @@ my_tesla = ElectricCar('tesla', 'model s', 2016)
 
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
