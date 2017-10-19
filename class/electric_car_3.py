309c6d0d7ac3df7e26b96c169e299b478a8877d7
delacroix <unmask429@gmail.com>
2017-10-19 22:10:22 +0800

    electric_car_3

--- class/electric_car_3.py
+++ class/electric_car_3.py
@@ -7,30 +7,30 @@ class Car(object):
 
     def get_descriptive_name(self):
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
-        return long_name.title()
+        return long_name
 
-    def read_odometer(self):
+    def update_odometer(self):
         print("This car has " + str(self.odometer_reading) + " miles on it.")
-
-    def update_odometer(self, mileage):
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
 
     def increment_odometer(self, miles):
         self.odometer_reading += miles
 
 
-class ElectricCar(Car):
-    def __init__(self, make, model, year):
-        super(ElectricCar, self).__init__(make, model, year)
-        self.battery_size = 70
+class Battery():
+    def __init__(self, battery_size=70):
+        self.battery_size = battery_size
 
     def describe_battery(self):
         print("This car has a " + str(self.battery_size) + "-kwh battery.")
 
 
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super(ElectricCar, self).__init__(make, model, year)
+        self.battery = Battery()
+
+
 my_tesla = ElectricCar('tesla', 'model s', 2016)
+
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
