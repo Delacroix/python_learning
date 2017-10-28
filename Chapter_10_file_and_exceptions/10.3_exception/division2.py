3d2e592d0dd3cceab4d74b3b94f07f15e4402705
unmask429@gmail.com <neverm0re@iot>
2017-10-28 15:12:35 +0800

    10.3_exception

--- Chapter_10_file_and_exceptions/10.3_exception/division2.py
+++ Chapter_10_file_and_exceptions/10.3_exception/division2.py
@@ -5,4 +5,8 @@ while True:
     first_number = raw_input("\nFirst number: ")
     if first_number == 'q':
         break
-    second_
+    second_number = raw_input("\nSecond number: ")
+    if second_number == 'q':
+        break
+    answer = int(first_number) / int(second_number)
+    print(answer)
