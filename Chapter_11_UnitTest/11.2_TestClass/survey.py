209bad3f7dd34c855de0d3b500c5eb6eb15e7eda
delacroix <unmask429@gmail.com>
2017-11-02 23:11:38 +0800

    11.2_test_class

--- Chapter_11_UnitTest/11.2_TestClass/survey.py
+++ Chapter_11_UnitTest/11.2_TestClass/survey.py
@@ -1,10 +1,21 @@
 class AnonymousSurvey():
     """Collecting anonymous survey"""
+
     def __init__(self, question):
-        """Storing a question, and ready for response"""
+        """Storing a question, and ready for responses"""
         self.question = question
         self.responses = []
 
-    def show_questions(self):
+    def show_question(self):
         """Show survey"""
-        print(question)
+        print(self.question)
+
+    def store_response(self, new_response):
+        """Storing a single survey"""
+        self.responses.append(new_response)
+
+    def show_results(self):
+        """Show all survey that collected"""
+        print("Survey results:")
+        for response in self.responses:
+            print('- ' + response)
